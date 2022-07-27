9c424f2b9da51b3653f4e7c5c99e82e6891f2323
AoifeWM <aoifewmulligan@gmail.com>
2022-07-26 21:43:38 -0700

    Added basic proof of concept ship movement and temorary sprite

--- game/app.py
+++ game/app.py
@@ -1,13 +1,56 @@
-class Ship():
+import os
+import pygame
+
+
+class Component:
 
     def __init__(self, x, y, img_path):
         self.x = x
         self.y = y
-        self.img_path = img_path
+        self.intended_x = x
+        self.intended_y = y
+        # self.img = pygame.image.load(os.path.join("assets", img_path))
+        self.img = pygame.transform.scale(pygame.image.load(os.path.join("assets", img_path)), (32, 32))
+        # self.facing = 0
 
-    def fly(self, distance):
-        self.x += distance
-        self.y += distance
+    def move(self, direction):
+        # self.x += distance
+        # self.y += distance
+        if direction == "left":
+            self.intended_x -= 16
+            # if self.facing != 3:
+            #   load left facing sprite into self.image
+            #   self.facing = 3
+        if direction == "right":
+            self.intended_x += 16
+        if direction == "up":
+            self.intended_y -= 16
+        if direction == "down":
+            self.intended_y += 16
+
+    def update(self):
+        if self.intended_x > self.x:
+            self.x += 2
+        if self.intended_x < self.x:
+            self.x -= 2
+        if self.intended_y > self.y:
+            self.y += 2
+        if self.intended_y < self.y:
+            self.y -= 2
 
 
-class Asteroid():
+
+    # def tile_to_pixel(self, x, y):
+    #     return (x*16, y*16)
+
+
+class Ship(Component):
+
+    def __init__(self, x, y, img_path):
+        super().__init__(x, y, img_path)
+
+    def render(self):
+        pass
+# class Asteroid():
+
+

67a27eeddc27526911b2c12e9e235f4ab5a5f35e
AoifeWM <aoifewmulligan@gmail.com>
2022-07-30 13:08:13 -0700

    Refactored random asteroid generation and collisions between disperate codebases

---
+++ sandbox/cleaned/app.py
@@ -0,0 +1,88 @@
+import os
+import pygame
+
+
+class Component:
+
+    def __init__(self, x, y, img_path, angle = 0):
+        self.angle = 90
+        self.x = x
+        self.y = y
+        self.intended_x = x
+        self.intended_y = y
+        # self.img = pygame.image.load(os.path.join("assets", img_path))
+        self.img = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(
+            os.path.join("assets", img_path)), (32, 32)), self.angle)
+        # self.facing = 0
+
+    def move(self, direction):
+        # self.x += distance
+        # self.y += distance
+        if direction == "left":
+            self.intended_x -= 16
+            # if self.facing != 3:
+            #   load left facing sprite into self.image
+            #   self.facing = 3
+        if direction == "right":
+            self.intended_x += 16
+        if direction == "up":
+            self.intended_y -= 16
+        if direction == "down":
+            self.intended_y += 16
+
+    def update(self):
+        if self.intended_x > self.x:
+            self.x += 2
+        if self.intended_x < self.x:
+            self.x -= 2
+        if self.intended_y > self.y:
+            self.y += 2
+        if self.intended_y < self.y:
+            self.y -= 2
+
+    # def tile_to_pixel(self, x, y):
+    #     return (x*16, y*16)
+
+
+class Ship(Component):
+    def __init__(self, x, y, img_path, angle = 0):
+        self.angle = 0
+        self.x = x
+        self.y = y
+        self.intended_x = x
+        self.intended_y = y
+        # self.img = pygame.image.load(os.path.join("assets", img_path))
+        self.img = pygame.transform.scale(pygame.image.load(
+            os.path.join("assets", img_path)), (48, 48))
+        # self.facing = 0
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def collision(self, obj):
+        return collide(self, obj)
+        
+
+    def rotate_img(self, angle):
+        self.angle = angle
+
+
+    def render(self):
+        pass
+
+class Asteroid(Component):
+    def __init__(self, x, y, img_path):
+        self.x = x
+        self.y = y
+        self.intended_x = x
+        self.intended_y = y
+        # self.img = pygame.image.load(os.path.join("assets", img_path))
+        self.img = pygame.transform.scale(pygame.image.load(
+            os.path.join("assets", img_path)), (96, 96))
+        # self.facing = 0
+        self.mask = pygame.mask.from_surface(self.img)
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    if obj1.mask.overlap(obj2.mask, (offset_x,offset_y)) != None:
+        print("collided")
+        return True
--- sandbox/cleaned/main.py
+++ sandbox/cleaned/main.py
@@ -5,6 +5,16 @@ from sprites import *
 from settings import *
 from tiles import *
 
+try:
+    from app import Ship, Asteroid
+except:
+    from game.app import Ship, Asteroid
+
+try:
+    from settings import *
+except:
+    from game.settings import *
+
 class Game:
 
     def __init__(self):
@@ -21,15 +31,35 @@ class Game:
         self.bg = pygame.transform.scale(self.bg, (960, 640))
         self.current_map = None
         self.current_space_map = None
+        self.score = 0
+        self.main_font = pygame.font.SysFont("Calibri", 40)
+        self.score_label = self.main_font.render(f"Score: {self.score}", True, (255, 255, 255))
+        # self.asteroid_one = Asteroid(random.randrange(100, WIN_WIDTH-100), random.randrange(100, WIN_HEIGHT-100), "MapTiles/zAstDebris03.png")
+        # self.asteroid_two = Asteroid(random.randrange(100, WIN_WIDTH-100), random.randrange(100, WIN_HEIGHT-100), "MapTiles/zAstDebris03.png")
+        # self.asteroid_three = Asteroid(random.randrange(100, WIN_WIDTH-100), random.randrange(100, WIN_HEIGHT-100), "MapTiles/zAstDebris03.png")
+
 
     def run(self):
         """run game"""
         self.playing = True
         self.all_sprites_group = pygame.sprite.LayeredUpdates()
         self.current_map_group = pygame.sprite.LayeredUpdates()
+        self.terrain_group = pygame.sprite.LayeredUpdates()
         # TODO: This is just a testmap for the first space map
         self.update_map("sandbox/cleaned/TestSpaceMap01.csv", True)
         self.current_space_map = self.current_map
+        self.switch_map(self.current_map)
+        # you have some tilemap object named NewMap.
+        # To render:
+        # NewMap.show_tiles()
+        # self.current_map = NewMap
+        # To unrender:
+        # self.current_map_group.empty()
+        # NewMap.group.empty()
+        # Typical situation: some other map is loaded, and you want to load your NewMap object:
+        # self.current_map_group.empty()
+        # NewMap.show_tiles()
+
 
         self.blocks = pygame.sprite.LayeredUpdates()
         self.enemies = pygame.sprite.LayeredUpdates()
@@ -56,6 +86,12 @@ class Game:
     def update(self):
         self.all_sprites_group.update()
         self.current_map_group.update()
+        self.terrain_group.update()
+        if self.player.player_is_ship:
+            for terrain in self.terrain_group:
+                if terrain.collide(self.player):
+                    self.switch_map(terrain.map)
+
 
     def draw(self):
         # TODO: Change this to load the tilemap
@@ -64,22 +100,38 @@ class Game:
         # self.tile_map.draw_map(self.screen)
         # self.screen.blit(self.tile_map.map_surface, (0, 0))
         self.current_map_group.draw(self.screen)
+        # self.screen.blit(self.asteroid_one.img, (self.asteroid_one.x, self.asteroid_one.y))
+        # self.screen.blit(self.asteroid_two.img, (self.asteroid_two.x, self.asteroid_two.y))
+        # self.screen.blit(self.asteroid_three.img, (self.asteroid_three.x, self.asteroid_three.y))
+        self.terrain_group.draw(self.screen)
         self.all_sprites_group.draw(self.screen)
+        self.screen.blit(self.score_label, (10, 10))
         self.clock.tick(FPS)
         pygame.display.update()
 
+    def switch_map(self, map):
+        self.current_map.unload_tiles()
+        self.current_map = map
+        self.current_map.show_tiles()
+
+
     def update_map(self, filepath, is_space_map):
         self.current_map_group.empty()
+        self.terrain_group.empty()
         self.current_map = None
-        self.current_map = TileMap(filepath, self.current_map_group, is_space_map)
+        self.current_map = TileMap(filepath, self.current_map_group, self.terrain_group, is_space_map)
         if is_space_map:
             self.current_space_map = self.current_map
 
     # TODO: rename
     def reload_space_map(self):
         # self.current_map.unload_tiles()
+        self.current_map_group.empty()
+        self.terrain_group.empty()
         self.current_map = self.current_space_map
         self.current_map.show_tiles()
+        self.player.rect.x = 96
+        self.player.rect.y = 96
         # self.current_map.reload_tiles()
 
     def main(self):

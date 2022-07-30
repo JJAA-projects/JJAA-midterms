1b19c59abfb076e4c9d8bb1ae551e6fb0cd2a1da
Anthony Turinski <71283758+Terransky@users.noreply.github.com>
2022-07-30 10:07:00 -0700

    sandbox: collision, camera following, intro, game over

--- sandbox/simpler/settings.py
+++ sandbox/simpler/settings.py
@@ -1,9 +1,27 @@
 
 
-WIN_WIDTH = 320
-WIN_HEIGHT = 320
+WIN_WIDTH = 160
+WIN_HEIGHT = 160
 TILESIZE = 16
-FPS = 30
+FPS = 60
+
+PLAYER_SPEED = 16
 
 PLAYER_LAYER = 9
+ENEMY_LAYER = 3
+UPGRADE_LAYER = 2
+BLOCK_LAYER = 2
 GROUND_LAYER = 1
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B...E.....U........B',
+    'B..................B',
+    'B..................B',
+    'B...P..............B',
+    'B...B..............B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]

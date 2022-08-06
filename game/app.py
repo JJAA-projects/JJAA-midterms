b39d957959faa1d351724f8d801621682e6d7cfe
Anthony Turinski <71283758+Terransky@users.noreply.github.com>
2022-08-06 14:15:52 -0700

    != equality operator changed to is not

--- game/app.py
+++ game/app.py
@@ -83,6 +83,6 @@ class Asteroid(Component):
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
-    if obj1.mask.overlap(obj2.mask, (offset_x,offset_y)) != None:
+    if obj1.mask.overlap(obj2.mask, (offset_x,offset_y)) is not None:
         print("collided")
         return True
